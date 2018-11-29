0de3941f8aa8f51c5d2855aa8027068978aae424
David Flynn <daflynn@csumb.edu>
2018-11-29 21:27:45 +0000

    First version

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Recipeat </title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script>function clearBox(elementID){document.getElementById(elementID).innerHTML = "";ingredients = [];}</script>
+        
+    </head>
+    <body>
+        <br/>
+        <div id = 'mainblock'>
+            <br/>
+            <h2 align = 'center'>Recipeat</h2>
+            <div id = 'line'></div>
+            
+            <br/>
+            
+            <select id = 'ddMenu'>
+              <option value="Eggs">Eggs</option>
+              <option value="Flour">Flour</option>
+              <option value="Milk">Milk</option>
+              <option value="Sugar">Sugar</option>
+            </select>
+            
+            <button type="button" id = 'confirm' onclick = 'conf()'>Add</button>
+            
+            <div id = 'basket'>test</div>
+            
+            
+            <button type="button" id = 'wipe' onclick = 'clearBox("basket")'>Clear</button>
+            
+            
+        
+            
+            
+            
+        </div>
+        
+        
+        
+        <script>
+            var ingredients = [];
+            
+            function conf() {
+                ingredients.push($('#ddMenu').val());
+                display();
+                
+            }
+            
+            function display() {
+                var out = '';
+                for (var i = 0; i < ingredients.length; i++) {
+                    out += ingredients[i];
+                    out += '<br/>';
+                }
+                
+                $("#basket").html(out);
+                
+                
+            }
+            
+            
+        </script>
+        
+
+    </body>
+</html>
