0c41278719871d2a9c320e7bf5938f9c246b8ffd
Jonathan Munz <jmunz@csumb.edu>
2018-12-09 17:20:37 +0000

    uploading files for login/logout function

--- home.php
+++ home.php
@@ -14,16 +14,21 @@ if(isset($_GET['tag']))
         <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
     </head>
     <body>
+        <?php include 'navigation.php' ?>
         <br><br><br>
         <header>
             <h1>Recipe Search</h1>
         </header>
+        
+        
         <main>
         <form>
             <input type="text" name="tag" placeholder = "Enter Ingredients" value="<?=$_GET['tag']?>"/>
             </br>
             <input type="Submit" value="Search"/><button id="trivia">test</button>
         </form>
+        
+        
         
         <?php
         if(empty($_GET)) // form was not submitted
@@ -48,7 +53,51 @@ if(isset($_GET['tag']))
             }
         }
         ?>
-        </main>
         
+        <div class="container">
+  <!--<h2>Modal Example</h2>-->
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Recipe</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Ingredients</h4>
+        </div>
+        <div class="modal-body">
+          <p>• Bread</p>
+          <p>• Milk</p>
+          <p>• Eggs</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+        </main>
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+</body>
+</html>
+
     </body>    
 </html>
